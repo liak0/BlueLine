09d74d5520ba93ba3af2d4ad00f5fa71b0f3513b
liak0 <2294760573@qq.com>
2021-02-17 08:47:48 -0500

    usb: gadget: Allow up to 900mA current draw for SuperSpeed
    Currently 500mA is used as max allowable current to draw over
    USB VBUS. However in SuperSpeed a gadget may draw up to 900mA
    Update the CONFIG_USB_GADGET_VBUS_DRAW's allowable range to
    be up to 900. But depending on connection speed, ensure that
    high speed and slower are capped to 500mA when encoding the
    bMaxPower value in the configuration descriptor.
    
    Change-Id: Iae9ecf586135b0a2064e7d5e6e8fa3d8e7e4fb70
    Signed-off-by: Jack Pham <jackp@codeaurora.org>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -595,13 +595,14 @@ static u8 encode_bMaxPower(enum usb_device_speed speed,
 		val = CONFIG_USB_GADGET_VBUS_DRAW;
 	if (!val)
 		return 0;
+
 	if (speed < USB_SPEED_SUPER)
-		return min(val, 500U) / 2;
+		return min(val, 900U) / 8;
 	else
 		/*
 		 * USB 3.x supports up to 900mA, but since 900 isn't divisible
-		 * by 8 the integral division will effectively cap to 896mA.
-		 */
+		 * by 8 the integral division will effectively cap to 896mA.*/
+		 
 		return min(val, 900U) / 8;
 }
 
